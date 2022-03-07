2601a7e7505300d02906e56e66067ee8c9db25eb
Leijk <18576525292@163.com>
2022-03-07 14:39:16 +0800

    测试

--- TestProject/app/src/main/java/com/example/testproject/MainActivity.kt
+++ TestProject/app/src/main/java/com/example/testproject/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //雷嘉康
         //test
         //yanshi
     }
